9f834a49a97edfcbfff25a5d22b4d43cacccebd2
Claire Pate <clairepate@Claires-Air.attlocal.net>
2017-12-18 20:00:35 -0500

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Bamazon
+
+Bamazon is a node application that is a simple e-commerce platform similar to Amazon. The application will take orders and will update the inventory in the mySQL database. 
+
+-bamazonCustomer.js- this is the client side that allows the customer to see the current 		inventory and make a purchase. 
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,125 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  console.log("--------------------");
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
+function start() {
+    connection.query('SELECT * FROM Products', function(err, res) {
+        console.log('Available Bamazon Products');
+        console.log('---------------------------------');
+            var table = new Table({
+                head: ['ID', 'ProductName', 'Department', 'Price', 'Quantity'],
+                colWidths: [7, 25, 20, 10, 10]
+            });
+        for (var i=0; i < res.length; i++) {
+            var productArray = [res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity];
+            table.push(productArray);
+        }
+        console.log(table.toString());
+        buyItem();
+        });
+    };
+
+
+// function which prompts the user for what action they should take
+function buyItem() {
+  inquirer.prompt([
+  {
+      name: "buying",
+      type: "input",
+      message: "What is the ID of the item that you would like to buy?",
+      validate: function(value) {
+            //validates answer
+        if (isNaN(value) === false) {
+            return true;
+        } else {
+            console.log("\nPlease enter only the Item number of the item you'd like to buy\n");
+            return false;
+            }
+      }
+    },{
+      name: "quantity",
+      type: "input",
+      message: "How many would you like to buy?",
+      validate: function(value){
+        if (isNaN(value) === false){
+            return true;
+        } else { 
+            console.log("\nPlease enter a valid quantity\n");
+            return false;
+        }
+      }
+    },
+    ])
+    .then(function(answer) {
+        var Item = parseInt(answer.stock_quantity);
+            //Queries the database
+            connection.query("SELECT * FROM products WHERE ?", {id: answer.id}, function(err, data) { 
+              if (err) throw err;
+              //Checks if sufficient quantity exists
+              if (data[0].stock_quantity < Item) {
+                console.log("We're sorry, that item is currently out of stock\n");
+                console.log("Please choose another product\n");
+                start(); 
+              } else {
+                  //if quantity exists updates database
+                  var updateQty = data[0].stock_quantity - Item;
+                  var totalPrice = data[0].price * Item;
+            connection.query('UPDATE products SET stock_quantity = ? WHERE id = ?', [updateQty, answer.id], function(err, results) {
+              if(err) {
+                  throw err;
+              } else {
+                  console.log("Purchase successfull!\n");
+                  console.log("Your total cost is: $ " + totalPrice);
+                  //Asks the buyer if they would like to continue
+                  inquirer.prompt({
+                    name: "buyMore",
+                    type: "confirm",
+                    message: "Would you like to buy another Product?",
+              }).then(function(answer) {
+                if (answer.buyMore === true) {
+                  start();
+                } else {
+                    console.log("Thank your for shopping with Bamazon!");
+                    connection.end();
+                  }
+            });
+                }
+              });
+            }               
+          });
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
